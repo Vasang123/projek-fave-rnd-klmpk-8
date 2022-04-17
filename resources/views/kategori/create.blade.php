eb1b368668468daf44dd511f3f4eb5530ea1af78
Valencius <Valencius>
2022-04-17 13:55:51 +0700

    category page

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -2,22 +2,33 @@
 
 
 @section('content')
-<div class="container col-md-5"  >
-    <div class="modal-header">
-        <h5 class="modal-title" id="tambahKategoriLabel">Tambah Kategori</h5>
+<div class="container col-md-6 mt-5"  >
+    <div class="container">
+        <h1 class="container" id="tambahKategoriLabel" style="color: #AD8B73;">Add New Category</h1>
     </div>
     <form action="{{ route('kategori.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="form-group mb-3">
-            <label for="nama_kategori">Nama Kategori</label>
+        <div class="form-group mb-3 mt-5">
+            <div class="row md-4">
+                <div class="col-md-4 d-flex justify-content-end align-items-center">
+                    <label for="nama_kategori"><b>Category Name</b></label>
+                </div>
+            <div class="col-md-8">
             <input type="text" class="form-control @error('nama_kategori') is-invalid @enderror"" placeholder="Masukkan nama kategori" name="nama_kategori" value="{{ old('nama_kategori') }}">
             @error('nama_kategori')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
+            </div>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary btn-sm text-white">Submit</button>
+        <div class="row">
+            <div class="col-md-4"></div>
+        <div class="col-md-8 d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary btn-md text-white mt-2">Submit</button>
+        </div>
+        </div>
     </form>
 </div>
 @endsection
--- resources/views/kategori/edit.blade.php
+++ resources/views/kategori/edit.blade.php
@@ -2,23 +2,33 @@
 
 
 @section('content')
-<div class="container col-md-5"  >
-    <div class="modal-header">
-        <h5 class="modal-title" id="tambahKategoriLabel">Edit Kategori</h5>
+<div class="container col-md-6 mt-5"  >
+    <div class="container">
+        <h1 class="container" id="tambahKategoriLabel" style="color: #AD8B73;">Edit Category</h1>
     </div>
     <form action="{{ route('kategori.update', $kategori->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('put')
-        <div class="form-group mb-3">
-            <label for="nama_kategori">Nama Kategori</label>
+        <div class="form-group mb-3 mt-5">
+            <div class="row">
+            <div class="col-md-4 d-flex justify-content-end align-items-center">
+            <label for="nama_kategori">Category Name</label>
+            </div>
+            <div class="col-md-8">
             <input type="text" class="form-control @error('nama_kategori') is-invalid @enderror"" placeholder="Masukkan nama kategori" name="nama_kategori" value="{{ $kategori->nama_kategori }}">
             @error('nama_kategori')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary btn-sm text-white">Update</button>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-8 d-flex justify-content-end">
+                <button type="submit" class="btn btn-primary btn-md text-white mt-2">Update</button>
+            </div>
     </form>
 </div>
 @endsection
